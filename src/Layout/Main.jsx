868cd6bbf079823689963c5536584e3bf116a90f
anamikagain559 <anamika.zaimahtech@gmail.com>
2024-05-30 19:59:17 +0600

    menu added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,18 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import NavBar from "../pages/Shared/NavBar/NavBar";
-import { useLocation } from "react-router-dom";
+
+
 const Main = () => {
-  const location = useLocation();
-  console.log(location);
-  const noHeaderFooter = location.pathname.includes("login");
-  return (
-    <div>
-      {noHeaderFooter || <NavBar></NavBar>}
-      <Outlet></Outlet>
-      {noHeaderFooter || <Footer></Footer>}
-    </div>
-  );
+    const location = useLocation();
+    
+    const noHeaderFooter = location.pathname.includes('login') || location.pathname.includes('signup');
+
+    return (
+        <div>
+            { noHeaderFooter || <NavBar></NavBar>}
+            <Outlet></Outlet>
+            { noHeaderFooter || <Footer></Footer>}
+        </div>
+    );
 };
 
 export default Main;
