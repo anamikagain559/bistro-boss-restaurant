bcfdd676f65dd8d0d5f6bcc85047d631f1cd3149
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-12-23 21:11:09 +0600

    Merge remote changes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -71,7 +71,7 @@ import UpdateItem from "../pages/Dashboard/UpdateItem/UpdateItem";
         {
           path: 'updateItem/:id',
           element: <AdminRoute><UpdateItem></UpdateItem></AdminRoute>,
-          loader: ({params}) => fetch(`http://localhost:5000/menu/${params.id}`)
+          loader: ({params}) => fetch(`https://bistro-boss-server-sigma-lake.vercel.app/menu/${params.id}`)
         },
         {
           path: 'users',
